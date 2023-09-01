211cc1dbaad3748124bde26de6c48d381dfdd383
Ralph <ralph.sleigh@gmail.com>
2023-09-01 16:36:09 +0100

    better build

--- esbuild-front-local.mjs
+++ esbuild-front-local.mjs
@@ -3,8 +3,8 @@ import * as esbuild from 'esbuild'
 await esbuild.build({
   entryPoints: ['src/front/index.tsx'],
   bundle: true,
-  minify: true,
-  sourcemap: false,
+  minify: false,
+  sourcemap: true,
   splitting: true,
   format: 'esm',
   //target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
@@ -19,5 +19,5 @@ await esbuild.build({
     '.svg': 'dataurl',
     '.gif': 'dataurl',
   },
-  watch: false
+  watch: true
 })
