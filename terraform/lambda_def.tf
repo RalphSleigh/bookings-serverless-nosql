c7a7db24fbe6c658217433bb723f3f734496d4e8
Ralph <ralph.sleigh@gmail.com>
2024-01-18 22:30:11 +0000

    fix terraform

--- terraform/lambda_def.tf
+++ terraform/lambda_def.tf
@@ -5,19 +5,19 @@ variable "lambda_definitions" {
   default = {
     /*migrate = {
       name                 = "migrate"
-      code_dir             = "migrate"
+      code             = "migrate"
       path                 = "migrate"
       dont_create_endpoint = true
     },
     init = {
       name                 = "init"
-      code_dir             = "init"
+      code             = "init"
       path                 = "init"
       dont_create_endpoint = true
     },
     seed = {
       name                 = "seed"
-      code_dir             = "seed"
+      code             = "seed"
       path                 = "seed"
       dont_create_endpoint = true
     },*/
@@ -120,19 +120,19 @@ variable "lambda_definitions" {
     /*handlerSetup('/api/event/:id/manage/timeline', 'events/manage/getTimeline')
     events_delete = {
       name     = "events_delete"
-      code_dir = "events/delete"
+      code = "events/delete"
       path     = "event/delete"
       method   = "post"
     },
     events_details = {
       name     = "events_details"
-      code_dir = "events/details"
+      code = "events/details"
       path     = "event/{id}/details"
       method   = "get"
     },
     apply = {
       name     = "apply"
-      code_dir = "events/apply"
+      code = "events/apply"
       path     = "event/{id}/apply"
       method   = "post"
     },*/
@@ -150,7 +150,7 @@ variable "lambda_definitions" {
     }, /*
     bookings_get = {
       name     = "bookings_get"
-      code_dir = "bookings/get"
+      code = "bookings/get"
       path     = "booking/{id}"
       method   = "get"
     },*/
@@ -159,106 +159,106 @@ variable "lambda_definitions" {
       code   = "bookings/editBooking"
       path   = "booking/edit"
       method = "post"
-    }, 
+    },
     bookings_delete = {
-      name     = "bookings_delete"
-      code_dir = "bookings/deleteBooking"
-      path     = "booking/delete"
-      method   = "post"
-    },/*
+      name   = "bookings_delete"
+      code   = "bookings/deleteBooking"
+      path   = "booking/delete"
+      method = "post"
+    }, /*
     bookings_syncmax = {
       name     = "bookings_syncmax"
-      code_dir = "bookings/syncmax"
+      code = "bookings/syncmax"
       path     = "booking/syncMax"
       method   = "post"
     },
     bookings_event = {
       name     = "bookings_event"
-      code_dir = "bookings/event"
+      code = "bookings/event"
       path     = "booking/event/{id}"
       method   = "get"
     },
     village_create = {
       name     = "village_create"
-      code_dir = "village/create"
+      code = "village/create"
       path     = "village/create"
       method   = "post"
     },
     village_delete = {
       name     = "village_delete"
-      code_dir = "village/delete"
+      code = "village/delete"
       path     = "village/delete"
       method   = "post"
     },
     village_assign = {
       name     = "village_assign"
-      code_dir = "village/assign"
+      code = "village/assign"
       path     = "village/assign"
       method   = "post"
     },
     village_rename = {
       name     = "village_rename"
-      code_dir = "village/rename"
+      code = "village/rename"
       path     = "village/rename"
       method   = "post"
     },
     application_decline = {
       name     = "application_decline"
-      code_dir = "application/decline"
+      code = "application/decline"
       path     = "application/decline"
       method   = "post"
     },
     application_approve = {
       name     = "application_approve"
-      code_dir = "application/approve"
+      code = "application/approve"
       path     = "application/approve"
       method   = "post"
     },
     role_create = {
       name     = "role_create"
-      code_dir = "role/create"
+      code = "role/create"
       path     = "role/create"
       method   = "post"
     },
     role_delete = {
       name     = "role_delete"
-      code_dir = "role/delete"
+      code = "role/delete"
       path     = "role/delete"
       method   = "post"
     },
     payment_add = {
       name     = "payment_add"
-      code_dir = "payment/add"
+      code = "payment/add"
       path     = "payment/add"
       method   = "post"
     },
     payment_delete = {
       name     = "payment_delete"
-      code_dir = "payment/delete"
+      code = "payment/delete"
       path     = "payment/delete"
       method   = "post"
     },
     membership_approve = {
       name     = "membership_approve"
-      code_dir = "membership/approve"
+      code = "membership/approve"
       path     = "membership/approve"
       method   = "post"
     },
     membership_unapprove = {
       name     = "membership_unapprove"
-      code_dir = "membership/unapprove"
+      code = "membership/unapprove"
       path     = "membership/unapprove"
       method   = "post"
     },
     dbs_approve = {
       name     = "dbs_approve"
-      code_dir = "dbs/approve"
+      code = "dbs/approve"
       path     = "dbs/approve"
       method   = "post"
     },
     dbs_unapprove = {
       name     = "dbs_unapprove"
-      code_dir = "dbs/unapprove"
+      code = "dbs/unapprove"
       path     = "dbs/unapprove"
       method   = "post"
     },*/
@@ -285,40 +285,40 @@ variable "lambda_definitions" {
       code   = "auth/google_drive/callback"
       path   = "auth/google_drive/callback"
       method = "get"
-    }, 
+    },
     auth_facebook_redirect = {
-      name     = "auth_facebook_redirect"
-      code_dir = "auth/facebook/redirect"
-      path     = "auth/facebook/redirect"
-      method   = "get"
+      name   = "auth_facebook_redirect"
+      code   = "auth/facebook/redirect"
+      path   = "auth/facebook/redirect"
+      method = "get"
     },
     auth_facebook_callback = {
-      name     = "auth_facebook_callback"
-      code_dir = "auth/facebook/callback"
-      path     = "auth/facebook/callback"
-      method   = "get"
-    },/*
+      name   = "auth_facebook_callback"
+      code   = "auth/facebook/callback"
+      path   = "auth/facebook/callback"
+      method = "get"
+    }, /*
     auth_yahoo_redirect = {
       name     = "auth_yahoo_redirect"
-      code_dir = "auth/yahoo/redirect"
+      code = "auth/yahoo/redirect"
       path     = "auth/yahoo/redirect"
       method   = "get"
     },
     auth_yahoo_callback = {
       name     = "auth_yahoo_callback"
-      code_dir = "auth/yahoo/callback"
+      code = "auth/yahoo/callback"
       path     = "auth/yahoo/callback"
       method   = "get"
     },
     auth_microsoft_redirect = {
       name     = "auth_microsoft_redirect"
-      code_dir = "auth/microsoft/redirect"
+      code = "auth/microsoft/redirect"
       path     = "auth/microsoft/redirect"
       method   = "get"
     },
     auth_microsoft_callback = {
       name     = "auth_microsoft_callback"
-      code_dir = "auth/microsoft/callback"
+      code = "auth/microsoft/callback"
       path     = "auth/microsoft/callback"
       method   = "get"
     },*/
