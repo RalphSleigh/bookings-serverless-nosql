9962d9d154244fcc3a2e0dcd00844a65bb99395a
Ralph <ralph.sleigh@gmail.com>
2024-01-20 17:44:02 +0000

    Final lambda

--- src/lambdas/email/email.ts
+++ src/lambdas/email/email.ts
@@ -1,15 +1,28 @@
 import { DeleteMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { SQSEvent } from "aws-lambda";
+import { get_config } from "../../lambda-common/config.js";
+import { sendEmail } from "../../lambda-common/email.js";
+import { flush_logs, log } from "../../lambda-common/logging.js"
 
- export const lambdaHandler = async (event: any): Promise<any> => {
-    console.log(event);
-
-    // Delete the event from SQS
+export const lambdaHandler = async (event: SQSEvent): Promise<any> => {
+    const config = await get_config()
     const client = new SQSClient({});
-    await client.send(
-        new DeleteMessageCommand({
-            QueueUrl: process.env.EMAIL_QUEUE_URL,
-            ReceiptHandle: event.ReceiptHandle,
-        }))
+    // Delete the event from SQS
 
+    for (const record of event.Records) {
+        const data = JSON.parse(record.body)
+        try {
+            await sendEmail(data, config)
+        } catch (error) {
+            log("Error sending email:")
+            log(error)
+        }
+        await client.send(
+            new DeleteMessageCommand({
+                QueueUrl: process.env.EMAIL_QUEUE_URL,
+                ReceiptHandle: record.receiptHandle,
+            }))
+    }
+    await flush_logs()
     return true;
 }
