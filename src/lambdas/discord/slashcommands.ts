fbcc01a1b2fefd3ce36411ae27acfd7a81264b0b
Ralph <ralph.sleigh@gmail.com>
2024-03-13 18:18:18 +0000

    update

--- src/lambdas/discord/slashcommands.ts
+++ src/lambdas/discord/slashcommands.ts
@@ -1,5 +1,5 @@
 import { Model } from 'dynamodb-onetable';
-import { lambda_wrapper_json } from '../../lambda-common/lambda_wrappers.js';
+import { lambda_wrapper_json, lambda_wrapper_raw } from '../../lambda-common/lambda_wrappers.js';
 import { BookingType, EventBookingTimelineType, EventType, OnetableBookingType, OnetableEventType, table } from '../../lambda-common/onetable.js';
 import { CanDeleteBooking, CanDeleteOwnBooking, CanEditBooking, CanEditEvent, PermissionError } from '../../shared/permissions.js';
 import { updateParticipantsDates } from '../../lambda-common/util.js';
@@ -7,9 +7,11 @@ import { queueDriveSync } from '../../lambda-common/drive_sync.js';
 import { queueManagerEmails } from '../../lambda-common/email.js';
 
 import { verify } from "discord-verify/node";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const lambdaHandler = lambda_wrapper_json(
-    async (lambda_event, config, current_user) => {
+
+export const lambdaHandler = async (lambda_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => { //@ts-ignore
+    return lambda_wrapper_raw(async (config) => {
         console.log(JSON.stringify(lambda_event))
-        if(lambda_event.body.type == 1)return { type: 1 }
     })
+}
