964c20f10ebacc49a41b0de1aa5d8eb7ebfca36f
Ralph <ralph.sleigh@gmail.com>
2024-03-13 19:11:01 +0000

    ok

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -127,7 +127,8 @@ resource "aws_cloudfront_origin_request_policy" "api_policy" {
     cookie_behavior = "all"
   }
   headers_config {
-    header_behavior = "all"
+    header_behavior = "allExcept"
+    headers {}
   }
 
   query_strings_config {
