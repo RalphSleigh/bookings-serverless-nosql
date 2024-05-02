b0d337a107b960392a6b186a8105453d2165ae9f
Ralph <ralph.sleigh@gmail.com>
2024-05-02 16:29:06 +0100

    Camp100 styling

--- src/front/app/toolbar.tsx
+++ src/front/app/toolbar.tsx
@@ -1,19 +1,28 @@
 import { Brightness4, Brightness7, BugReport } from "@mui/icons-material";
-import { AppBar, Box, Button, IconButton, Link, Toolbar, Typography, useTheme } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Link, Toolbar, Typography, createTheme, useTheme } from "@mui/material";
 import React, { useContext } from "react";
 import { ThemeContext } from "./themeContext.js";
 import { UserStatus } from "../user/userStatus.js";
 import { EnvContext } from "./envContext.js";
+import { ThemeProvider } from "@emotion/react";
 
 export function AppToolbar(props) {
-    const theme = useTheme();
+    const globalTheme = useTheme()
     const colourMode = useContext(ThemeContext)
     const env = useContext(EnvContext)
     const [error, setError] = React.useState(false);
 
     if (error) throw ("BOOM (render)")
-
-    return (<AppBar position="static">
+    const theme = createTheme({
+            palette: {
+                primary: {
+                    main: "#e9e1ca",
+                    contrastText: 'rgba(0,0,0,0.7)',
+                }
+          }
+        })
+    return (<ThemeProvider theme={globalTheme.palette.mode == "light" ? theme : globalTheme}>
+        <AppBar position="static">
         <Toolbar variant="dense">
                 <Box
                     component="img"
@@ -23,7 +32,7 @@ export function AppToolbar(props) {
                         mr: 1
                     }}
                     alt="Logo"
-                    src="/logoonly.png"
+                    src="/header-wordmark.png"
                 />
                 <Link sx={{ flexShrink: 1, minWidth: 0, overflow: 'hidden' }} noWrap={true} underline="hover" variant="h6" color="inherit" href="/">
                     {window.location.hostname}
@@ -42,5 +51,6 @@ export function AppToolbar(props) {
                 <Typography variant="h6" sx={{ ml: 1, color: "warning.main" }}>TEST MODE</Typography>
             </> : null}
         </Toolbar>
-    </AppBar >)
+    </AppBar >
+    </ThemeProvider>)
 }
