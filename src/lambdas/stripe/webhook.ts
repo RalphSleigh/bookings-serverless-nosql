6dffcd6ed6303cd9b301bcacdd713d73405f653a
Ralph <ralph.sleigh@gmail.com>
2024-06-07 15:57:17 +0100

    discord stripe payment

--- src/lambdas/stripe/webhook.ts
+++ src/lambdas/stripe/webhook.ts
@@ -4,6 +4,7 @@ import { BookingType, EventBookingTimelineType, EventType, OnetableBookingType, 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { Jsonify } from 'type-fest';
 import Stripe from 'stripe';
+import { postToDiscord } from '../../lambda-common/discord.js';
 
 const BookingModel: Model<OnetableBookingType> = table.getModel<OnetableBookingType>('Booking')
 
@@ -34,6 +35,8 @@ export const lambdaHandler = async (lambda_event: APIGatewayProxyEvent): Promise
                                 set: { fees: 'list_append(if_not_exists(fees, @{emptyList}), @{newFees})' },
                                 substitutions: { emptyList: [], newFees: fees }
                             })
+
+                await postToDiscord(config, `Payment of £${paymentIntent.amount_received/100} received from booking ${booking.basic.contactName}`)
             }
         }
         return { statusCode: 200 }
