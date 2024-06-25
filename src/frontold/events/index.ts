b8a6ed8ea99abaff2f4cdece7062b136383ac87d
Ralph <ralph.sleigh@gmail.com>
2024-06-25 15:37:06 +0100

    Remove old code

--- src/frontold/events/index.ts
+++
@@ -1,8 +0,0 @@
-import * as actions from './actions'
-import reducer from './reducer'
-
-import listPage from './components/listPage'
-import createPage from './components/createPage'
-import editPage from './components/editPage'
-
-export { actions, reducer, listPage, createPage, editPage };
